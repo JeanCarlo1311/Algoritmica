3c9f42a45adb6dedc1b0057241e1d127600c9ed3
Jean Carlo Solano N <jeancarlosolanon@gmail.com>
2024-10-27 01:16:47 -0700

    Delete holamundo2.cxx

--- holamundo2.cxx
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	printf("hola mundo");
-	printf("\n hola mundo");	//hola//
-	printf("\nX	\nY");	//mundo//
-	printf("\n	\n	hola munda");	//JeanCalo//
-	printf("\n	\n	\n	\"hola mundo\"");
-}
